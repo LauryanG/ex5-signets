5858a5c5dd4c2ea7dc4f55076844d4596b13c62c
Lauryan <lauryan-gervais@hotmail.com>
2021-03-31 21:54:18 -0400

    Firebase hosting

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -3,9 +3,34 @@ import Dossier from './Dossier';
 import * as crudDossiers from '../services/crud-dossiers';
 import { useState, useEffect } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 export default function ListeDossiers({utilisateur, etatDossiers}) {
   // État des dossiers (vient du composant Appli)
   const [dossiers, setDossiers] = etatDossiers;
+
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+  
+    const classes = useStyles();
+    const [trie, setTrie] = useState('');
+  
+    const handleChange = (event) => {
+      setTrie(event.target.value);
+    };
+  
 
   // Lire les dossiers dans Firestore et forcer le réaffichage du composant
   // Remarquez que ce code est dans un useEffect() car on veut l'exécuter 
@@ -37,6 +62,27 @@ export default function ListeDossiers({utilisateur, etatDossiers}) {
   
   return (
     <>
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Trie des dossiers
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>Défaut</em>
+          </MenuItem>
+          <MenuItem value={10}>la date de modification descendante</MenuItem>
+          <MenuItem value={20}>le nom de dossier alphabétique ascendant</MenuItem>
+          <MenuItem value={30}>le nom de dossier alphabétique descendant</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
     <ul className="ListeDossiers">
       {
         (dossiers.length > 0) ?
--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,14 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-
+  apiKey: "AIzaSyDYXixKoQkDQRBQM6g_v0lspnYwWsUV2OM",
+  authDomain: "veille-technologique-173fb.firebaseapp.com",
+  databaseURL: "https://veille-technologique-173fb-default-rtdb.firebaseio.com",
+  projectId: "veille-technologique-173fb",
+  storageBucket: "veille-technologique-173fb.appspot.com",
+  messagingSenderId: "884338243671",
+  appId: "1:884338243671:web:1e216f22b558fb53a98b12",
+  measurementId: "G-95QWWJ1VMS"
 };
 
 export default firebaseConfig;
